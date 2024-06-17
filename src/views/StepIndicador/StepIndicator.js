14047eb33721d7d5b48cd57c225c0175c3987e9b
RuiYuriAfricano <yuriafricano03@gmail.com>
2024-06-17 02:49:08 +0100

    feat: add step indicator

---
+++ src/views/StepIndicador/StepIndicator.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import './StepIndicator.css';
+
+const StepIndicator = ({ currentStep, vetor }) => {
+  return (
+    <div className="step-indicator">
+      {vetor.map((stepNumber) => (
+        <div
+          key={stepNumber}
+          className={`step ${currentStep >= stepNumber.id ? 'active' : ''} ${currentStep > stepNumber.id ? 'completed' : ''}`}
+        >
+          <div className="step-icon">{stepNumber.id}</div>
+          <div className="step-name">{stepNumber.txt}</div>
+        </div>
+      ))}
+    </div>
+  );
+};
+
+export default StepIndicator;
