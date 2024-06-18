44099125efb3c66091b11730717db2ff7517af7f
RuiYuriAfricano <yuriafricano03@gmail.com>
2024-06-18 09:58:02 +0100

    chore: alteracao na _nav.js

--- src/_nav.js
+++ src/_nav.js
@@ -1,5 +1,5 @@
-import React from 'react'
-import CIcon from '@coreui/icons-react'
+import React, { useState, useEffect } from 'react';
+import CIcon from '@coreui/icons-react';
 import {
   cilBell,
   cilCalculator,
@@ -24,98 +24,182 @@ import {
   cilShare,
   cilSpeedometer,
   cilStar,
+  cilUser,
   cilUserFollow,
   cilUserPlus,
+  cilUserUnfollow,
+  cilUserX,
   cilWifiSignal0,
   cilWifiSignal1,
   cilWifiSignal4,
   cilWifiSignalOff,
-} from '@coreui/icons'
-import { CNavGroup, CNavItem, CNavTitle } from '@coreui/react'
+} from '@coreui/icons';
+import { CNavGroup, CNavItem, CNavTitle } from '@coreui/react';
 
-const _nav = [
-  {
-    component: CNavItem,
-    name: 'Home',
-    to: '/dashboard',
-    icon: <CIcon icon={cilHome} customClassName="nav-icon" />,
-  },
-  {
-    component: CNavTitle,
-    name: 'Conteúdo Multimidia',
-  },
-  {
-    component: CNavItem,
-    name: 'Gerir Conteúdo',
-    to: '/gerirConteudo',
-    icon: <CIcon icon={cilList} customClassName="nav-icon" />,
-  },
-  {
-    component: CNavItem,
-    name: 'Pesquisar Conteúdo',
-    to: '/theme/typography',
-    icon: <CIcon icon={cilSearch} customClassName="nav-icon" />,
-  },
-  {
-    component: CNavTitle,
-    name: 'Avançado',
-  },
-  {
-    component: CNavItem,
-    name: 'Grupos De Amigos',
-    to: '/grupoDeAmigos',
-    icon: <CIcon icon={cilGroup} customClassName="nav-icon" />,
-  },
-  {
-    component: CNavItem,
-    name: 'Grupos Musicais',
-    to: '/grupoMusical',
-    icon: <CIcon icon={cilMusicNote} customClassName="nav-icon" />,
-  },
-  {
-    component: CNavItem,
-    name: 'PlayLists',
-    to: '/playlist',
-    icon: <CIcon icon={cilFeaturedPlaylist} customClassName="nav-icon" />,
-  },
-  {
-    component: CNavItem,
-    name: 'Upload',
-    to: '/charts',
-    icon: <CIcon icon={cilCloudUpload} customClassName="nav-icon" />,
-  },
-  {
-    component: CNavItem,
-    name: 'Partilhar Conteúdo',
-    to: '/charts',
-    icon: <CIcon icon={cilShare} customClassName="nav-icon" />,
-  },
-  {
-    component: CNavItem,
-    name: 'Downloads',
-    to: '/charts',
-    icon: <CIcon icon={cilCloudDownload} customClassName="nav-icon" />,
-  },
-  {
-    component: CNavItem,
-    name: 'Lista de Partilhas',
-    to: '/charts',
-    icon: <CIcon icon={cilList} customClassName="nav-icon" />,
-  },
-  {
-    component: CNavItem,
-    name: 'Artistas',
-    to: '/artistas',
-    icon: <CIcon icon={cilPeople} customClassName="nav-icon" />,
-  },
+const DynamicNav = () => {
+  const [user, setUser] = useState(null);
+  const [navItems, setNavItems] = useState([]);
 
-  {
-    component: CNavItem,
-    name: 'Rádio',
-    to: '/estacoesDeRadio',
-    icon: <CIcon icon={cilWifiSignal1} customClassName="nav-icon" />,
-  },
+  useEffect(() => {
+    const loggedUser = JSON.parse(localStorage.getItem("loggedUser"));
+    setUser(loggedUser);
+  }, []);
 
-]
+  useEffect(() => {
+    if (user) {
+      const adminNav = [
+        {
+          component: CNavItem,
+          name: 'Home',
+          to: '/dashboard',
+          icon: <CIcon icon={cilHome} customClassName="nav-icon" />,
+        },
+        {
+          component: CNavTitle,
+          name: 'Conteúdo Multimidia',
+        },
+        {
+          component: CNavItem,
+          name: 'Gerir Conteúdo',
+          to: '/gerirConteudo',
+          icon: <CIcon icon={cilList} customClassName="nav-icon" />,
+        },
+        {
+          component: CNavItem,
+          name: 'Pesquisar Conteúdo',
+          to: '/theme/typography',
+          icon: <CIcon icon={cilSearch} customClassName="nav-icon" />,
+        },
+        {
+          component: CNavTitle,
+          name: 'Avançado',
+        },
+        {
+          component: CNavItem,
+          name: 'Grupos De Amigos',
+          to: '/grupoDeAmigos',
+          icon: <CIcon icon={cilGroup} customClassName="nav-icon" />,
+        },
+        {
+          component: CNavItem,
+          name: 'Grupos Musicais',
+          to: '/grupoMusical',
+          icon: <CIcon icon={cilMusicNote} customClassName="nav-icon" />,
+        },
+        {
+          component: CNavItem,
+          name: 'PlayLists',
+          to: '/playlist',
+          icon: <CIcon icon={cilFeaturedPlaylist} customClassName="nav-icon" />,
+        },
+        {
+          component: CNavItem,
+          name: 'Downloads',
+          to: '/charts',
+          icon: <CIcon icon={cilCloudDownload} customClassName="nav-icon" />,
+        },
+        {
+          component: CNavItem,
+          name: 'Lista de Partilhas',
+          to: '/charts',
+          icon: <CIcon icon={cilList} customClassName="nav-icon" />,
+        },
+        {
+          component: CNavItem,
+          name: 'Utilizadores',
+          to: '/charts',
+          icon: <CIcon icon={cilUser} customClassName="nav-icon" />,
+        },
+        {
+          component: CNavItem,
+          name: 'Artistas',
+          to: '/artistas',
+          icon: <CIcon icon={cilPeople} customClassName="nav-icon" />,
+        },
+        {
+          component: CNavItem,
+          name: 'Rádio',
+          to: '/estacoesDeRadio',
+          icon: <CIcon icon={cilWifiSignal1} customClassName="nav-icon" />,
+        },
+      ];
 
-export default _nav
+      const userNav = [
+        {
+          component: CNavItem,
+          name: 'Home',
+          to: '/dashboard',
+          icon: <CIcon icon={cilHome} customClassName="nav-icon" />,
+        },
+        {
+          component: CNavTitle,
+          name: 'Conteúdo Multimidia',
+        },
+        {
+          component: CNavItem,
+          name: 'Gerir Conteúdo',
+          to: '/gerirConteudo',
+          icon: <CIcon icon={cilList} customClassName="nav-icon" />,
+        },
+        {
+          component: CNavItem,
+          name: 'Pesquisar Conteúdo',
+          to: '/theme/typography',
+          icon: <CIcon icon={cilSearch} customClassName="nav-icon" />,
+        },
+        {
+          component: CNavTitle,
+          name: 'Avançado',
+        },
+        {
+          component: CNavItem,
+          name: 'Grupos De Amigos',
+          to: '/grupoDeAmigos',
+          icon: <CIcon icon={cilGroup} customClassName="nav-icon" />,
+        },
+        {
+          component: CNavItem,
+          name: 'Grupos Musicais',
+          to: '/grupoMusical',
+          icon: <CIcon icon={cilMusicNote} customClassName="nav-icon" />,
+        },
+        {
+          component: CNavItem,
+          name: 'PlayLists',
+          to: '/playlist',
+          icon: <CIcon icon={cilFeaturedPlaylist} customClassName="nav-icon" />,
+        },
+        {
+          component: CNavItem,
+          name: 'Downloads',
+          to: '/charts',
+          icon: <CIcon icon={cilCloudDownload} customClassName="nav-icon" />,
+        },
+        {
+          component: CNavItem,
+          name: 'Lista de Partilhas',
+          to: '/charts',
+          icon: <CIcon icon={cilList} customClassName="nav-icon" />,
+        },
+        {
+          component: CNavItem,
+          name: 'Artistas',
+          to: '/artistas',
+          icon: <CIcon icon={cilPeople} customClassName="nav-icon" />,
+        },
+        {
+          component: CNavItem,
+          name: 'Rádio',
+          to: '/estacoesDeRadio',
+          icon: <CIcon icon={cilWifiSignal1} customClassName="nav-icon" />,
+        },
+      ];
+
+      setNavItems(user.tipoDeUtilizador === 'admin' ? adminNav : userNav);
+    }
+  }, [user]);
+
+  return navItems;
+};
+
+export default DynamicNav;
