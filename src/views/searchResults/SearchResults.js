91c5349d0680f803282e0a7bac72474812f2e11c
RuiYuriAfricano <yuriafricano03@gmail.com>
2024-06-25 12:53:38 +0100

    chore: add search componente

--- src/views/searchResults/SearchResults.js
+++ src/views/searchResults/SearchResults.js
@@ -1,50 +1,154 @@
 // SearchResults.js
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
 import './SearchResults.css'
+import { service } from './../../services';
+import { isNumber } from 'chart.js/helpers';
+import { useNavigate } from 'react-router-dom'; // Import useNavigate
+
+
 
 const SearchResults = () => {
+    const navigate = useNavigate(); // Initialize useNavigate
     const { query } = useParams();
+    const [searchResults2, setSearchResults2] = useState([])
 
-    // Mock data for search results, replace with actual data fetching logic
-    const results = [
-        {
-            id: 1,
-            title: 'Say The Word - Hillsong UNITED - Live in Israel',
-            views: '296k views',
-            timestamp: '5 years ago',
-            image: 'https://img.youtube.com/vi/xyz/0.jpg', // Replace with actual image URL
-            author: 'Hillsong UNITED',
-            authorImage: 'https://source.unsplash.com/random/36x36?profile' // Replace with actual author image URL
-        },
-        {
-            id: 2,
-            title: 'Say The Word - Music Video - Hillsong UNITED',
-            views: '6.7M views',
-            timestamp: '8 years ago',
-            image: 'https://img.youtube.com/vi/xyz/0.jpg', // Replace with actual image URL
-            author: 'Hillsong UNITED',
-            authorImage: 'https://yt3.ggpht.com/ytc/AKedOLQXZ5R1eN1nGZ-HlT8yzvnJmZz2hvz1a8C9=s88-c-k-c0x00ffffff-no-rj' // Replace with actual author image URL
-        },
-    ];
+    const handleSearch = async () => {
+
+        // Simulate a search request to get videos
+        const response = await service.album.pesquisarVideosMusicasEAlbunsDoGrupoPorTitulo(query);
+        setSearchResults2(response.data.success ? response.data.data : []);
+        console.log(response.data.data)
+
+    };
+    useEffect(() => {
+        handleSearch()
+    }, [query])
+
+    const timeElapsed = (date) => {
+        const now = new Date();
+        const past = new Date(date);
+
+        const diffInMs = now - past;
+
+        const seconds = Math.floor(diffInMs / 1000);
+        const minutes = Math.floor(seconds / 60);
+        const hours = Math.floor(minutes / 60);
+        const days = Math.floor(hours / 24);
+        const months = Math.floor(days / 30.44); // Approximation
+        const years = Math.floor(days / 365.25); // Approximation
+
+        if (years > 0) {
+            return { value: years, unit: 'anos' };
+        } else if (months > 0) {
+            return { value: months, unit: 'meses' };
+        } else if (days > 0) {
+            return { value: days, unit: 'dias' };
+        } else if (hours > 0) {
+            return { value: hours, unit: 'horas' };
+        } else if (minutes > 0) {
+            return { value: minutes, unit: 'minutos' };
+        } else {
+            return { value: seconds, unit: 'segundos' };
+        }
+    };
+
+    const handleMouseEnter = (event, result) => {
+        if (result.tipo === 'video') {
+            const videoElement = event.currentTarget.querySelector('.video-preview');
+            const thumbnailElement = event.currentTarget.querySelector('.thumbnail');
+            thumbnailElement.style.display = 'none';
+            videoElement.style.display = 'block';
+            videoElement.play();
+        }
+    };
+
+    const handleMouseLeave = (event, result) => {
+        if (result.tipo === 'video') {
+            const videoElement = event.currentTarget.querySelector('.video-preview');
+            const thumbnailElement = event.currentTarget.querySelector('.thumbnail');
+            thumbnailElement.style.display = 'block';
+            videoElement.style.display = 'none';
+            videoElement.pause();
+            videoElement.currentTime = 0; // Reset video to start
+        }
+    };
+
+    const handleClick = (result) => { // Function to handle click
+        navigate(`/play/${result.tipo}/${result.codigo}`);
+    };
 
     return (
         <div className="search-results">
             <h4>Resultados para "{query}"</h4>
             <div className="results-list">
-                {results.map((result) => (
-                    <div key={result.id} className="result-item">
-                        <img className="thumbnail" src={result.image} alt={result.title} />
-                        <div className="result-info">
-                            <img className="author-image" src={result.authorImage} alt={result.author} />
-                            <div className="result-details">
-                                <h3>{result.title}</h3>
-                                <p className="author-name">{result.author}</p>
-                                <p className="views-timestamp">{result.views} • {result.timestamp}</p>
+                {searchResults2.map((result, index) => {
+                    if (result.tipo === 'video') {
+                        return (
+                            <div
+                                key={result.codigo + "-" + result.tipo}
+                                className="result-item"
+                                onClick={() => handleClick(result)}
+                                onMouseEnter={(e) => handleMouseEnter(e, result)}
+                                onMouseLeave={(e) => handleMouseLeave(e, result)}
+                                style={{ cursor: 'pointer' }}
+                            >
+                                <img className="thumbnail"
+                                    src={`http://localhost:3333/video/${result.codigo}/thumbnail`}
+                                    alt={result.titulo}
+                                />
+                                {result.tipo === 'video' && (
+                                    <video
+                                        className="video-preview"
+                                        src={`http://localhost:3333/video/downloadVideo/${result.codigo}`}
+                                        style={{ display: 'none', width: '500px', height: '300px', borderRadius: '10px' }}
+                                        muted
+                                    />
+                                )}
+                                <div className="result-info">
+                                    <img className="author-image" src={'http://localhost:3333/utilizador/download/' + 'RuiMalemba'} alt={result.fkArtista ? result.artista.nomeArtita : result.grupoMusical.nomeGrupoMusical} />
+                                    <div className="result-details">
+                                        <h3>{result.titulo}</h3>
+                                        <p className="author-name">{isNumber(result.fkArtista) ? result.artista.nomeArtista : result.grupoMusical.nomeGrupoMusical}</p>
+                                        <p className="views-timestamp">6.7M views • {"Há " + timeElapsed(result.dataDeRegisto).value + " " + timeElapsed(result.dataDeRegisto).unit}</p>
+                                    </div>
+                                </div>
                             </div>
-                        </div>
-                    </div>
-                ))}
+                        )
+                    }
+                    else if (result.tipo === 'musica') {
+                        return (
+                            <div key={result.id} className="result-item">
+                                <img className="thumbnail" src={`http://localhost:3333/musica/downloadCapa/${result.codigo}`} alt={result.titulo} />
+                                <div className="result-info">
+                                    <img className="author-image" src={'http://localhost:3333/utilizador/download/' + 'RuiMalemba'} alt={result.fkArtista ? result.artista.nomeArtita : result.grupoMusical.nomeGrupoMusical} />
+                                    <div className="result-details">
+                                        <h3>{result.titulo}</h3>
+                                        <p className="author-name">{isNumber(result.fkArtista) ? result.artista.nomeArtista : result.grupoMusical.nomeGrupoMusical}</p>
+                                        <p className="views-timestamp">6.7M views • {"Há " + timeElapsed(result.dataDeRegisto).value + " " + timeElapsed(result.dataDeRegisto).unit}</p>
+                                    </div>
+                                </div>
+                            </div>
+                        )
+                    }
+                    else if (result.tipo === 'album') {
+                        return (
+                            <div key={result.id} className="result-item">
+                                <img className="thumbnail" src={`http://localhost:3333/album/downloadCapa/${result.codigo}`} alt={result.titulo} />
+                                <div className="result-info">
+                                    <img className="author-image" src={'http://localhost:3333/utilizador/download/' + 'RuiMalemba'} alt={result.fkArtista ? result.artista.nomeArtita : result.grupoMusical.nomeGrupoMusical} />
+                                    <div className="result-details">
+                                        <h3>{result.titulo}</h3>
+                                        <p className="author-name">{isNumber(result.fkArtista) ? result.artista.nomeArtista : result.grupoMusical.nomeGrupoMusical}</p>
+                                        <p className="views-timestamp">6.7M views • {"Há " + timeElapsed(result.dataDeRegisto).value + " " + timeElapsed(result.dataDeRegisto).unit}</p>
+                                    </div>
+                                </div>
+                            </div>
+                        )
+                    }
+                }
+
+                )}
             </div>
         </div>
     );
